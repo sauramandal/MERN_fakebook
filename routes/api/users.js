3685e64c60cb25b77972da2bf8873ef39de0f532
montai <saura.mandal1@gmail.com>
2019-08-13 16:22:11 +0530

    Added users registration route and tested the same

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,18 +19,18 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters')
         .isLength({ min: 6 })
 ], async(req, res) => { 
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
     try {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const { name, email, password } = req.body;
-        console.log(req.body);
         let user = await User.findOne({ email });
-        console.log(user);
         
         if(user) {
-            res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            return  res.status(400).json({ errors: [{ msg: 'User already exists' }] });
         }
         const avatar = gravatar.url(email, {
             s: '200',
@@ -44,27 +44,15 @@ router.post('/', [
             avatar,
             password
         });
-
-        const salt = await bcrypt.getSalt(10);
+        
+        const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-        jwt.sign(
-            payload, 
-            config.get('jwtSecret'),
-            { expiresIn: 36000 },
-            (err, token) => {
-                if(err) throw err;
-                res.send({ token });
-            });
+        res.send('User Route');
     } catch(err) {
         console.log(err.message);
         res.status(500).send('Server Error');
-    }  
+    }
 });
 
 module.exports = router;
