183ac67b194e030114ba100a15e5108f7eca5f38
montai <saura.mandal1@gmail.com>
2019-08-14 15:37:51 +0530

    Created user profile with req objects & setted findUseAndModify to false

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -50,7 +50,7 @@ router.post(
             location,
             bio,
             status,
-            githubstatus,
+            githubusername,
             skills,
             youtube,
             facebook,
@@ -62,7 +62,47 @@ router.post(
         //Build profile object
         const profileFields = {};
         profileFields.user = req.user.id;
+        if(company) profileFields.company = company;
+        if(website) profileFields.website = website;
+        if(location) profileFields.location = location;
+        if(bio) profileFields.bio = bio;
+        if(status) profileFields.status = status;
+        if(githubusername) profileFields.githubusername = githubusername;
+        if(skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+
+        //Build social object
+        profileFields.social = {};
+        if(youtube) profileFields.social.youtube = youtube;
+        if(twitter) profileFields.social.twitter = twitter;
+        if(facebook) profileFields.social.facebook = facebook;
+        if(linkedin) profileFields.social.linkedin = linkedin;
+        if(instagram) profileFields.social.instagram = instagram;
         
+        try {
+            let profile = await Profile.findOne({ user: req.user.id });
+            if(profile) {
+                //Update
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id }, 
+                    { $set: profileFields },
+                    { new: true }
+                );
+                res.json({profile});
+            } 
+            //Create new Profile
+            profile = new Profile(profileFields);
+            await profile.save();
+            res.json(profile);
+
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+        console.log(profileFields.skills);
+        res.send('Hello');
+
 });
 
 module.exports = router; 
